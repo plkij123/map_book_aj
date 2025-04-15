8332383fe1329aa3b90650da22b96ebe93650cc6
plkij123 <u64733@student.wat.edu.pl>
2025-04-15 17:53:37 +0200

    wip: add menu

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from utils.controler import get_user_info
 
 
 def main():
-    get_user_info(users)
+    print('================MENU================')
+    print('0 - zakoncz program')
+    print('1 - wyświetl co u znajomych')
+    print('====================================')
+    while True:
+        choice: str=input('wybierz opcje menu: ')
+        if choice == '0':break
+        if choice == '1':get_user_info(users)
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     main()
